b4a2d8222d1ae54215fbaeabcfc786f6b71c6c42
Derek <dphtran501@yahoo.com>
2017-10-15 01:40:10 -0700

    renamed MainActivity and activity_main.xml to AnimationActivity and activity_animation.xml, respectively; finished activity_animation.xml

--- app/src/main/java/edu/orangecoastcollege/cs273/dtran258/northernlightsanimation/AnimationActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/dtran258/northernlightsanimation/AnimationActivity.java
@@ -3,13 +3,13 @@ package edu.orangecoastcollege.cs273.dtran258.northernlightsanimation;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity
+public class AnimationActivity extends AppCompatActivity
 {
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_animation);
     }
 }
